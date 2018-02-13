a7f2b9d46172e4fd38a3505949e7f23122fe28de
Mateusz Stomski <lehronn@gmail.com>
2018-02-13 21:59:20 +0100

    3.42+

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,52 @@ import './App.css';
 import Person from './Person/Person.jsx'
 
 class App extends Component {
+  state = {
+    persons: [
+      {name:'Mateusz', age:'17'},
+      {name:'Aleksandra', age:'21'},
+      {name:'Iwona', age:'51'},
+      {name:'Jerzy', age:'23'}
+    ]
+  }
+
+  switchNameHandler = (newName, newAge) => {
+    // console.log('Was clicked.');
+this.setState({persons: [
+  {name : newName, age : '24'},
+  {name : 'Stephanie', age : '52'},
+  {name : 'Ami', age : '4'},
+  {name : 'Noe', age : '12'}
+]})
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person name = "Mateusz" age = "35" />
-        <Person name = "Aleksandra" age = "41" />
-        <Person name = "Jerzy" age = "25" />
-        <Person name = "Iwona" age = "16" />
-        <Person name = "Ludwik" age = "125" />
+        <button onClick={() => this.switchNameHandler()(this, 'Max')}>Switch name</button>
+        <Person
+          name = {this.state.persons[0].name}
+          age = {this.state.persons[0].age}
+          click = {this.switchNameHandler.bind(this, 'Stan')}>
+          My hobbies: Racing</Person>
+        <Person
+          name = {this.state.persons[1].name}
+          age = {this.state.persons[1].age}
+          click = {this.switchNameHandler.bind(this, 'Ron')}>
+          My hobbies : Games </Person>
+        <Person
+          name = {this.state.persons[2].name}
+          age = {this.state.persons[2].age}
+          click = {this.switchNameHandler.bind(this, 'Harry')}>
+          My hobbies : Music </Person>
+        <Person
+          name = "Iwona"
+          age = "16"/>
+        <Person
+          name = "Ludwik"
+          age = "125">
+          My hobbies : Dance </Person>
       </div>
     );
   }
--- src/Person/Person.jsx
+++ src/Person/Person.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Person.css';
+
+//props.children is a content inside <Component>...</Component.
 
 const person = (props) => {
   return (
-    <div>
-      <p>Hi, I'am {props.name} and i {props.age} years old.</p>
+    <div className="person">
+      <p onClick={props.click}>Hi, I'am {props.name} and I' am {props.age} years old.</p>
+      <p>{props.children}</p>
     </div>
   )
 };
